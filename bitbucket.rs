803ff36b636b65cb82e763bcc11a4a8ac51a08ed
Nathan Wilcox <nejucomo@gmail.com>
2013-09-28 16:37:21 -0700

    Move bitfifo-specific BitBucket operations into a private internal module (along with unittests).

--- bitbucket.rs
+++ bitbucket.rs
@@ -1,5 +1,3 @@
-use std::uint;
-
 use BitCount;
 
 
@@ -13,54 +11,5 @@ pub struct BitBucket {
 impl BitBucket {
     pub fn new() -> BitBucket {
         BitBucket { bits: 0, count: 0 }
-    }
-
-    pub fn merge_left(&self, other: BitBucket, count: BitCount) -> (BitBucket, BitBucket) {
-        assert_le!(count, uint::bits);
-        assert_le!(self.count, count); // If this were false, use pop_bits instead.
-
-        let total = self.count + other.count;
-
-        if count >= total {
-            // Shove all bits to the left:
-            (BitBucket {
-                    bits: (self.bits << other.count) | other.bits,
-                    count: total
-                },
-             BitBucket::new())
-
-        } else {
-            let tomove = count - self.count;
-            let keepright = other.count - tomove;
-
-            (BitBucket {
-                    bits: (self.bits << tomove) | other.bits >> keepright,
-                    count: count
-                },
-             BitBucket {
-                    bits: other.bits & ((1 << keepright) - 1),
-                    count: keepright
-                })
-        }
-    }
-
-    pub fn pop_bits(&mut self, count: BitCount) -> BitBucket {
-        if (count == 0u) {
-            return BitBucket::new();
-        }
-
-        assert_le!(count, self.count);
-
-        let keep = self.count.checked_sub(&count).unwrap();
-
-        let result = BitBucket {
-            bits: self.bits >> keep,
-            count: count
-        };
-
-        self.bits = self.bits & ((1u << keep) - 1);
-        self.count = keep;
-
-        result
     }
 }
--- bitfifo.rs
+++ bitfifo.rs
@@ -58,7 +58,7 @@ impl BitFifo {
         let total = self.incoming.count + source.count;
         assert_le!(total, 2 * uint::bits);
 
-        let (a, b) = self.incoming.merge_left(source, uint::bits);
+        let (a, b) = bbops::merge_left(self.incoming, source, uint::bits);
 
         if a.count == uint::bits {
             self.queue.push_back(a.bits);
@@ -73,10 +73,11 @@ impl BitFifo {
         assert_le!(count, self.count());
 
         if count <= self.outgoing.count {
-            self.outgoing.pop_bits(count)
+            let outgoing = &mut self.outgoing;
+            bbops::pop_bits(outgoing, count)
         } else {
             let tmp = self.pop_internal_bitbucket();
-            let (a, b) = self.outgoing.merge_left(tmp, count);
+            let (a, b) = bbops::merge_left(self.outgoing, tmp, count);
             self.outgoing = b;
             a
         }
@@ -92,6 +93,135 @@ impl BitFifo {
 
             Some(bits) => {
                 BitBucket { bits: bits, count: uint::bits }
+            }
+        }
+    }
+}
+
+
+// Private bitbucket operations:
+mod bbops {
+    use std::uint;
+    use BitCount;
+    use bitbucket::BitBucket;
+
+    pub fn merge_left(left: BitBucket, right: BitBucket, count: BitCount) -> (BitBucket, BitBucket) {
+        assert_le!(count, uint::bits);
+        assert_le!(left.count, count); // If this were false, use pop_bits instead.
+
+        let total = left.count + right.count;
+
+        if count >= total {
+            // Shove all bits to the left:
+            (BitBucket {
+                    bits: (left.bits << right.count) | right.bits,
+                    count: total
+                },
+             BitBucket::new())
+
+        } else {
+            let tomove = count - left.count;
+            let keepright = right.count - tomove;
+
+            (BitBucket {
+                    bits: (left.bits << tomove) | right.bits >> keepright,
+                    count: count
+                },
+             BitBucket {
+                    bits: right.bits & ((1 << keepright) - 1),
+                    count: keepright
+                })
+        }
+    }
+
+    pub fn pop_bits(source: &mut BitBucket, count: BitCount) -> BitBucket {
+        if (count == 0u) {
+            return BitBucket::new();
+        }
+
+        assert_le!(count, source.count);
+
+        let keep = source.count.checked_sub(&count).unwrap();
+
+        let result = BitBucket {
+            bits: source.bits >> keep,
+            count: count
+        };
+
+        source.bits = source.bits & ((1u << keep) - 1);
+        source.count = keep;
+
+        result
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use self::utils::*;
+        use super::pop_bits;
+
+        #[test]
+        fn pop_bits_0() {
+            use std::uint;
+            use bitbucket::BitBucket;
+
+            let src = BitBucket { bits: 0x0123456789abcdef, count: uint::bits };
+            let mut bb = src.clone();
+            assert_eq!(BitBucket::new(), pop_bits(&mut bb, 0));
+            assert_eq!(src, bb);
+        }
+
+        #[test] fn all_in_all_out() { iotest(shift_in_all, shift_out_all) }
+        #[test] fn all_in_chunked_out() { iotest(shift_in_all, shift_out_chunked) }
+        #[test] fn chunked_in_all_out() { iotest(shift_in_chunked, shift_out_all) }
+        #[test] fn chunked_in_chunked_out() { iotest(shift_in_chunked, shift_out_chunked) }
+
+
+        mod utils {
+            use bitbucket::BitBucket;
+            use super::super::{merge_left, pop_bits};
+
+            static full: BitBucket = BitBucket { bits: 0x1b, count: 5 };
+            static chunks: [BitBucket, .. 3] = [
+                BitBucket { bits: 0x3, count: 2 },
+                BitBucket { bits: 0x1, count: 2 },
+                BitBucket { bits: 0x1, count: 1 },
+                ];
+
+            pub fn iotest(inop: &fn(&mut BitBucket), outop: &fn(&mut BitBucket)) {
+                let bb = &mut BitBucket::new();
+                inop(bb);
+                outop(bb);
+            }
+
+            pub fn shift_in_all(dest: &mut BitBucket) {
+                shift_in(dest, full);
+                assert_eq!(*dest, full);
+            }
+
+            pub fn shift_in_chunked(dest: &mut BitBucket) {
+                for c in chunks.iter() {
+                    shift_in(dest, *c);
+                }
+                assert_eq!(*dest, full);
+            }
+
+            pub fn shift_out_all(src: &mut BitBucket) {
+                let out = pop_bits(src, full.count);
+                assert_eq!(out, full);
+                assert_eq!(*src, BitBucket::new());
+            }
+
+            pub fn shift_out_chunked(src: &mut BitBucket) {
+                for c in chunks.iter() {
+                    let out = pop_bits(src, c.count);
+                    assert_eq!(*c, out);
+                }
+            }
+
+            fn shift_in(dest: &mut BitBucket, source: BitBucket) {
+                let (a, b) = merge_left(*dest, source, dest.count + source.count);
+                assert_eq!(b, BitBucket::new());
+                *dest = a;
             }
         }
     }
